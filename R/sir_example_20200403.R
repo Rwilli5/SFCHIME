d6909c2467e811d0c638c802b2ecf7c017d1d234
Gabriel Odom <godom@fiu.edu>
2020-04-03 17:25:09 -0400

    added Anshul's original code (since I modified it so heavily).

--- R/sir_example_20200403.R
+++ R/sir_example_20200403.R
@@ -2,76 +2,197 @@
 # Anshul Saxena
 # 2020-04-03
 
+# Gabriel Odom cleaned this code on 2020-04-03
+
 
 library(deSolve)
 library(tidyverse)
 library(lubridate)
 
-data_all <- read.csv("https://urldefense.proofpoint.com/v2/url?u=https-3A__raw.githubusercontent.com_nytimes_covid-2D19-2Ddata_master_us-2Dcounties.csv&d=DwIGAg&c=lhMMI368wojMYNABHh1gQQ&r=mVr-r9NPAqGQx9-ub1jree0Ze-CRIXG5uPNvt5ADyuo&m=dbqn59Hx7Y17l9DeUN6GwXoVLlwr1CbQr7Hdzdr4DOA&s=Uq_LcDFn4p2V_0zHo9zxyJHzs_8cl1e5e5iZzUQFa6M&e= ")
+
+######  Import and Explore Data  ##############################################
+data_all <- read.csv(
+  "https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv"
+)
 
 # Can also use JHU source, that one is more recent
 
-mdc <- data_all %>% filter(state=='Florida') %>% filter(fips==12086) %>%
-  mutate(incidence = c(0, diff(cases)), time=(1:length(cases)), Date=date(date))
-
-data1=mdc
-
-
-SIR <- function(time, state, parameters) {
-  par <- as.list(c(state, parameters))
-  with(par, {
-    dS <- -beta/N * I * S
-    dI <- beta/N * I * S - gamma * I
-    dR <- gamma * I
-    list(c(dS, dI, dR))
-  })
-}
-
-Infected <- data1$cases
-Day <- 1:(length(Infected))
+dataMDC <- data_all %>%
+  filter(fips == 12086) %>%
+  mutate(
+    incidence = c(0, diff(cases)),
+    day       = 1:length(cases),
+    Date      = date(date)
+  ) %>% 
+  select(-date, -county, -state, -fips)
 
 
+par(mfrow = c(1, 2))
+plot(dataMDC$day, dataMDC$cases, type ="b")
+plot(dataMDC$day, dataMDC$cases, log = "y", ylab = "Infected (log scaled)")
+abline(lm(log10(dataMDC$cases) ~ dataMDC$day))
+title(
+  "Confirmed COVID Cases in Miami-Dade (2020-04-01)",
+  outer = TRUE,
+  line = -2
+)
 
+
+
+######  Set Up Optimisation  ##################################################
 N <- 2752000 # population of MDC
 
+init <- c(
+  S = N - dataMDC$cases[1],
+  I = dataMDC$cases[1],
+  R = 0
+)
 
-old <- par(mfrow = c(1, 2))
-plot(Day, Infected, type ="b")
-plot(Day, Infected, log = "y", ylab = "Infected (log scaled)")
-abline(lm(log10(Infected) ~ Day))
-title("Confirmed COVID Cases in Miami-Dade (2020-04-01)", outer = TRUE, line = -2)
-
-
-init <- c(S = N-Infected[1], I = Infected[1], R = 0)
-RSS <- function(parameters) {
-  names(parameters) <- c("beta", "gamma")
-  out <- ode(y = init, times = Day, func = SIR, parms = parameters)
-  fit <- out[ , 3]
-  sum((Infected - fit)^2)
+SIR <- function(time, state, parameters) {
+  
+  par <- as.list(c(state, parameters))
+  
+  with(
+    data = par,
+    expr = {
+      
+      dS <- -beta/N * I * S
+      dI <- beta/N * I * S - gamma * I
+      dR <- gamma * I
+      list(c(dS, dI, dR))
+      
+    }
+  )
+  
 }
 
-Opt <- optim(c(0.5, 0.5), RSS, method = "L-BFGS-B", lower = c(0, 0), upper = c(1, 1)) 
+RSS <- function(parameters, .f, initialVals, response, day_int) {
+  
+  names(parameters) <- c("beta", "gamma")
+  
+  out <- ode(
+    y = initialVals,
+    times = day_int,
+    func = .f,
+    parms = parameters
+  )
+  
+  fit <- out[ , 3]
+  sum((response - fit) ^ 2)
+  
+}
 
-Opt$message
-## [1] "CONVERGENCE: REL_REDUCTION_OF_F <= FACTR*EPSMCH"
+algorithms <- c(
+  NelderMead = "Nelder-Mead",
+  QuasiNewton = "BFGS",
+  ConjGrad = "CG",
+  BddQuasiNewton = "L-BFGS-B",
+  SimAnneal = "SANN"
+)
 
-Opt_par <- setNames(Opt$par, c("beta", "gamma"))
-Opt_par
+fitMessages <- vector("list", length = length(algorithms))
+
+optParams <- vector("list", length = length(algorithms))
+
+R0 <- rep(NA_real_, length = length(algorithms))
+
+names(R0) <- names(fitMessages) <- names(optParams) <- names(algorithms)
 
 
-t <- 1:70 # time in days
-fit <- data.frame(ode(y = init, times = t, func = SIR, parms = Opt_par))
-col <- 1:3 # colour
 
-matplot(fit$time, fit[ , 2:4], type = "l", xlab = "Day", ylab = "Number of subjects", lwd = 2, lty = 1, col = col)
-matplot(fit$time, fit[ , 2:4], type = "l", xlab = "Day", ylab = "Number of subjects (log scaled)", lwd = 2, lty = 1, col = col, log = "y")
+######  Fit Predictions  ######################################################
+col <- 1:3
+for (m in 3:length(algorithms)) {
+  # Nelder-Mead and Quasi-Newton are bad; if you get negative values from 
+  #   simulated annealing, run it again.
+  # browser()
+  
+  alg <- algorithms[m]
+  
+  ###  Optimimal Slution  ###
+  if(alg != "L-BFGS-B"){
+    Opt <- optim(
+      par = c(0.5, 0.5),
+      fn = RSS,
+      .f = SIR,
+      initialVals = init,
+      response = dataMDC$cases,
+      day_int = dataMDC$day,
+      method = alg
+    ) 
+  } else {
+    Opt <- optim(
+      par = c(0.5, 0.5),
+      fn = RSS,
+      .f = SIR,
+      initialVals = init,
+      response = dataMDC$cases,
+      day_int = dataMDC$day,
+      method = alg,
+      lower = c(0, 0),
+      upper = c(5, 5)
+    ) 
+  }
+  
+  fitMessages[[m]] <- Opt$message
+  ## [1] "CONVERGENCE: REL_REDUCTION_OF_F <= FACTR*EPSMCH"
+  # "ERROR: ABNORMAL_TERMINATION_IN_LNSRCH"
+  
+  Opt_par <- setNames(Opt$par, c("beta", "gamma"))
+  
+  optParams[[m]] <- Opt_par
+  
+  R0[m] <- Opt_par["beta"] / Opt_par["gamma"]
+  
+  
+  ###  Plot Prediction  ###
+  fit <- data.frame(
+    ode(y = init, times = 1:70, func = SIR, parms = Opt_par)
+  )
+  
+  matplot(
+    fit$time, fit[ , 2:4],
+    type = "l",
+    xlab = "Day",
+    ylab = "Number of subjects",
+    lwd = 2,
+    lty = 1,
+    col = col
+  )
+  matplot(
+    fit$time, fit[ , 2:4],
+    type = "l",
+    xlab = "Day",
+    ylab = "Number of subjects (log scaled)",
+    lwd = 2,
+    lty = 1,
+    col = col,
+    log = "y"
+  )
+  points(dataMDC$day, dataMDC$cases)
+  legend(
+    "bottomright",
+    c("Susceptible", "Infected", "Recovered"),
+    lty = 1,
+    lwd = 2,
+    col = col,
+    inset = 0.05
+  )
+  title(
+    paste(
+      "SIR model for total COVID cases in Miami-Dade (2020-04-01) using",
+      names(alg),
+      "Numeric Optimization"
+    ),
+    outer = TRUE,
+    line = -2
+  )
+  
+  # END for()
+}
 
+###  Optimal Parameters and R0  ###
+# Conjugate Gradients:  beta = 0.6856, gamma = 0.3144, R0 = 2.18
+# Bounded Quasi-Newton: beta = 0.8342, gamma = 0.4629, R0 = 1.80
+# Simulated Annealing:  beta = 1.0870, gamma = 0.7152, R0 = 1.52
 
-points(Day, Infected)
-legend("bottomright", c("Susceptible", "Infected", "Recovered"), lty = 1, lwd = 2, col = col, inset = 0.05)
-title("SIR model for total COVID cases in Miami-Dade (2020-04-01)", outer = TRUE, line = -2)
-
-par(old)
-
-R0 <- setNames(Opt_par["beta"] / Opt_par["gamma"], "R0")
-R0
---
+++ R/sir_example_original.R
@@ -0,0 +1,72 @@
+library(deSolve)
+library(tidyverse)
+library(lubridate)
+
+data_all <- read.csv("https://urldefense.proofpoint.com/v2/url?u=https-3A__raw.githubusercontent.com_nytimes_covid-2D19-2Ddata_master_us-2Dcounties.csv&d=DwIGAg&c=lhMMI368wojMYNABHh1gQQ&r=mVr-r9NPAqGQx9-ub1jree0Ze-CRIXG5uPNvt5ADyuo&m=dbqn59Hx7Y17l9DeUN6GwXoVLlwr1CbQr7Hdzdr4DOA&s=Uq_LcDFn4p2V_0zHo9zxyJHzs_8cl1e5e5iZzUQFa6M&e= ")
+
+# Can also use JHU source, that one is more recent
+
+mdc <- data_all %>% filter(state=='Florida') %>% filter(fips==12086) %>%
+  mutate(incidence = c(0, diff(cases)), time=(1:length(cases)), Date=date(date))
+
+data1=mdc
+
+
+SIR <- function(time, state, parameters) {
+  par <- as.list(c(state, parameters))
+  with(par, {
+    dS <- -beta/N * I * S
+    dI <- beta/N * I * S - gamma * I
+    dR <- gamma * I
+    list(c(dS, dI, dR))
+  })
+}
+
+Infected <- data1$cases
+Day <- 1:(length(Infected))
+
+
+
+N <- 2752000 # population of MDC
+
+
+old <- par(mfrow = c(1, 2))
+plot(Day, Infected, type ="b")
+plot(Day, Infected, log = "y", ylab = "Infected (log scaled)")
+abline(lm(log10(Infected) ~ Day))
+title("Confirmed COVID Cases in Miami-Dade (2020-04-01)", outer = TRUE, line = -2)
+
+
+init <- c(S = N-Infected[1], I = Infected[1], R = 0)
+RSS <- function(parameters) {
+  names(parameters) <- c("beta", "gamma")
+  out <- ode(y = init, times = Day, func = SIR, parms = parameters)
+  fit <- out[ , 3]
+  sum((Infected - fit)^2)
+}
+
+Opt <- optim(c(0.5, 0.5), RSS, method = "L-BFGS-B", lower = c(0, 0), upper = c(1, 1)) 
+
+Opt$message
+## [1] "CONVERGENCE: REL_REDUCTION_OF_F <= FACTR*EPSMCH"
+
+Opt_par <- setNames(Opt$par, c("beta", "gamma"))
+Opt_par
+
+
+t <- 1:70 # time in days
+fit <- data.frame(ode(y = init, times = t, func = SIR, parms = Opt_par))
+col <- 1:3 # colour
+
+matplot(fit$time, fit[ , 2:4], type = "l", xlab = "Day", ylab = "Number of subjects", lwd = 2, lty = 1, col = col)
+matplot(fit$time, fit[ , 2:4], type = "l", xlab = "Day", ylab = "Number of subjects (log scaled)", lwd = 2, lty = 1, col = col, log = "y")
+
+
+points(Day, Infected)
+legend("bottomright", c("Susceptible", "Infected", "Recovered"), lty = 1, lwd = 2, col = col, inset = 0.05)
+title("SIR model for total COVID cases in Miami-Dade (2020-04-01)", outer = TRUE, line = -2)
+
+par(old)
+
+R0 <- setNames(Opt_par["beta"] / Opt_par["gamma"], "R0")
+R0
