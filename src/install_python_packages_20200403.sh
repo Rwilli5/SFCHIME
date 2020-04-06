4d03fce67c708a1460f062ccab53dec35beaad46
gabrielodom <gabriel_odom@baylor.edu>
2020-04-06 14:31:52 -0400

    Running code inside of RMarkdown script is ok (for the setup; I haven't gotten to the good stuff yet).

--- src/install_python_packages_20200403.sh
+++ src/install_python_packages_20200403.sh
@@ -9,8 +9,8 @@
 # After much pain and suffering, Tim Norris helped me set up my Conda environment.
 
 # For macOS:
-conda create --name env_sflCHARM python=3.6 pandas numpy altair
-source activate env_sflCHARM
+conda create --name env_sflCHIME python=3.6 pandas numpy altair
+source activate env_sflCHIME
 pip install streamlit
 
 # For Windows:
@@ -27,18 +27,9 @@ pip install streamlit
 # conda activate env_sflCHARM
 # pip install streamlit
 
-# pip install datetime
-# pip install collections
-# # pip install typing
-# conda install typing
-# pip install abc
-# conda install abc
-# # conda update -n base conda
-# 
-# # for Anshul's script
-# pip install functools
-# # pip install pandas
-# conda install pandas
-# pip install streamlit
-# pip install numpy
-# pip install altair
+
+# Python complains that I don't have a Java Developer's Toolkit (JDK). Install
+#   is not trivial, because Oracle hides the link to the .dmg file:
+# https://www.oracle.com/java/technologies/javase-jdk14-downloads.html
+# Also, read these instructions (DURING install):
+# https://docs.oracle.com/en/java/javase/14/install/installation-jdk-macos.html#GUID-F575EB4A-70D3-4AB4-A20E-DBE95171AB5F
